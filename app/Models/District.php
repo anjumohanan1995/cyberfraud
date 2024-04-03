fe19a806f4efe1c9b72a010fd2f6f2fdb540d2bb
anjumohanan1995 <anjumohanan@kawikatechnologies.com>
2024-04-03 16:33:45 +0530

    changes

--- app/Models/District.php
+++ app/Models/District.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent; 
 
-class District extends Model
+class District extends Eloquent
 {
     use HasFactory;
 }
