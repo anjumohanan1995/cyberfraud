ca92390516545eb2d3ce2fa7e22088e8dc4210ad
anjumohanan1995 <anjumohanan@kawikatechnologies.com>
2024-04-08 11:15:10 +0530

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
            // SuperAdminSeeder::class,
            // RoleSeeder::class,
-           DistrictSeeder::class,
+          // DistrictSeeder::class,
+          PermissionSeeder::class,
 
          ]);
     }
