ffbb73a0453fcd787fda2768f6f207824a1ec9d4
Rajmohan-Kawika <raj@kawikatechnologies.com>
2024-05-20 16:52:44 +0530

    minor fix

--- resources/views/dashboard/bank-case-data/index.blade.php
+++ resources/views/dashboard/bank-case-data/index.blade.php
@@ -12,10 +12,10 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a href="#">Case Data Management</a>
+                            <a href="#">Upload CaseData</a>
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">
-                            Case Data
+                            Bank CaseData Upload!
                         </li>
                     </ol>
                 </nav>
@@ -35,6 +35,16 @@
                         <div class="card-body">
                             <div class=" m-4 d-flex justify-content-between">
 
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
                                 @if (session('success'))
                                     <div class="alert alert-success alert-dismissible fade show w-100" role="alert">
                                         {{ session('success') }}
@@ -43,176 +53,60 @@
                                         </button>
                                     </div>
                                 @endif
-                                <div class="alert alert-success-one alert-dismissible fade show w-100" role="alert"
-                                    style="display:none">
-
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
                             </div>
+
+
                             <div class=" m-4 d-flex justify-content-between">
                                 <h4 class="card-title mg-b-10">
-                                    All Case Data
+                                    Add Bank data!
                                 </h4>
-                                <div class="col-md-1 col-6 text-center">
-                                    <div class="task-box primary  mb-0">
-                                        <a class="text-white" href="{{ route('modus.create') }}">
-                                            <p class="mb-0 tx-12">Add </p>
-                                            <h3 class="mb-0"><i class="fa fa-plus"></i></h3>
-                                        </a>
-                                    </div>
-                                </div>
                             </div>
 
-                            <form id="complaint-form">
-                                <div class="row">
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                      <label for="from-date">From Date:</label>
-                                      <input type="date" class="form-control" id="from-date" name="from_date">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                      <label for="to-date">To Date:</label>
-                                      <input type="date" class="form-control" id="to-date" name="to_date">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                      <label for="mobile">Complaint Mobile no: </label>
-                                      <input type="text" class="form-control" id="mobile" name="mobile">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="type">Transaction Type:</label><br>
-                                        <select class="form-control" id="type">
-                                            <option>--Select--</option>
-                                            <option value="bank">Bank</option>
-                                            <option value="wallet">Wallet/PG/PA</option>
-                                            <option value="merchant">Merchant</option>
-                                            <option value="tsurunc">Insurance</option>
-                                        </select>
-                                        <br>
-
-                                    </div>
-                                  </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label for="options">Bank/Wallet/Merchant/Insurance:</label>
-                                        <select class="form-control" id="options"></select>
-                                        </select>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                      <label for="filled-by">Filled by(within 24 hrs):</label>
-                                      <select class="form-control" id="filled-by" name="filled-by">
-                                        <option value="">All</option>
-                                        <option value="citizen">Citizen</option>
-                                        <option value="cyber">Cyber</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3">
-                                    <div class="form-group">
-                                      <label for="acknowledgement_no">Acknowledgement No: </label>
-                                      <input type="text" class="form-control" id="acknowledgement_no" name="acknowledgement_no">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3" hidden>
-                                    <div class="form-group">
-                                      <label for="complaint-reported">Complaint Reported:</label>
-                                      <select class="form-control" id="complaint-reported" name="complaint-reported">
-                                        <option value="">All</option>
-                                        <option value="#">Through Helpline(1930)</option>
-                                        <option value="#">Cyber Crime Portal</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3" hidden>
-                                    <div class="form-group">
-                                      <label for="fir-lodge">FIR Lodge:</label>
-                                      <select class="form-control" id="fir-lodge" name="fir-lodge">
-                                        <option value="">--Select--</option>
-                                        <option value="#">Yes</option>
-                                        <option value="#">No</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="row">
-                                  <div class="col-md-3" hidden>
-                                    <div class="form-group">
-                                      <label for="acknowledgement_no">Acknowledgement No: </label>
-                                      <input type="text" class="form-control" id="acknowledgement_no" name="acknowledgement_no">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3" hidden>
-                                    <div class="form-group">
-                                      <label for="sub-category">Sub category:</label>
-                                      <select class="form-control" id="sub-category" name="sub-category">
-                                        <option value="">--Select--</option>
-                                        <option value="#">Yes</option>
-                                        <option value="#">No</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                  <div class="col-md-3" hidden>
-                                    <div class="form-group">
-                                      <label for="search-by">Search by:</label>
-                                      <select class="form-control" id="search-by" name="search-by">
-                                        <option value="">--Select--</option>
-                                        <option value="account_id">Account ID/Account Number/UPI ID</option>
-                                        <option value="transaction_id">Transaction ID/UTR/RRN Number</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-12 text-right">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-
                             <div class="table-responsive mb-0">
-                                <table id="example"
-                                    class="table table-hover table-bordered mb-0 text-md-nowrap text-lg-nowrap text-xl-nowrap table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>SL No</th>
-                                            <th>Source Type</th>
-                                            <th>Acknowledgement No</th>
-                                            <th>District</th>
-                                            <th>Police Station</th>
-                                            <th>Complainant Name</th>
-                                            <th>Complainant Mobile</th>
-                                            <th>Transaction ID</th>
-                                            <th>Bank Name</th>
-                                            <th>Account ID</th>
-                                            <th>Amount</th>
-                                            <th>Entry Date</th>
-                                            <th>Current Status</th>
-                                            <th>Date of Action</th>
-                                            <th>Action Taken By Name</th>
-                                            <th>Action Taken By Designation</th>
-                                            <th>Action Taken By Mobile</th>
-                                            <th>Action Taken By Email</th>
-                                            <th>Action Taken By Bank</th>
+                                <form action="{{ route('bank-case-data.store') }}" method="POST"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{-- <div class="form-group">
+                                                <label for="source_type">Source Type:</label>
+                                                <select class="form-control" id="source_type" name="source_type">
+                                                    <option value="NCRP Portal"
+                                                        {{ old('source_type') == 'NCRP Portal' ? 'selected' : '' }}>NCRP
+                                                        Portal</option>
+                                                    <option value="Public"
+                                                        {{ old('source_type') == 'Public' ? 'selected' : '' }}>Public
+                                                    </option>
+                                                    <option value="Cyber Dome"
+                                                        {{ old('source_type') == 'Cyber Dome' ? 'selected' : '' }}>Cyber
+                                                        Dome</option>
+                                                    <option value="Special Branch"
+                                                        {{ old('source_type') == 'Special Branch' ? 'selected' : '' }}>
+                                                        Special Branch</option>
+                                                    <option value="Cyber Operation"
+                                                        {{ old('source_type') == 'Cyber Operation' ? 'selected' : '' }}>
+                                                        Cyber Operation</option>
+                                                </select>
+                                                @error('source_type')
+                                                    <div class="text-danger">{{ $message }}</div>
+                                                @enderror
+                                            </div> --}}
 
-                                            <th>ACTION</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
+                                            <div class="form-group">
+                                                <label for="source_type">Upload Excel:</label>
+                                                <input type="file" name="file" id="file" name="file">
+                                                @error('file')
+                                                    <div class="text-danger">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+
+
+
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+
                             </div>
                         </div>
                     </div>
@@ -224,135 +118,4 @@
         </div>
         <!-- /row -->
     </div>
-
-    <script>
-        $("#type").on('change', function() {
-            var type = document.getElementById("type").value;
-            var dropdown = document.getElementById("options");
-
-            dropdown.innerHTML = ""; // Clear previous options
-
-            if (type === "bank") {
-                // Assuming `banks` is defined somewhere in your code
-                var banks = @json($banks); // Convert the PHP array to JSON
-
-                banks.forEach(function(bank) {
-                    var option = document.createElement("option");
-                    option.text = bank.name; // Accessing the `name` property of each bank
-                    dropdown.add(option);
-                });
-            }
-            // Add similar logic for other types if needed
-        });
-    </script>
-
-     <script>
-$(document).ready(function() {
-
-    var table = $('#example').DataTable({
-        processing: true,
-        serverSide: true,
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
-        ],
-        ajax: {
-            url: "{{ route('get.datalist') }}",
-            data: function(d) {
-                return $.extend({}, d, {});
-            }
-        },
-        columns: [
-            { data: 'id' },
-            { data: 'source_type' },
-            {
-                        data: 'acknowledgement_no'
-                    },
-                    {
-                        data: 'district'
-                    },
-                    {
-                        data: 'police_station'
-                    },
-                    {
-                        data: 'complainant_name'
-                    },
-                    {
-                        data: 'complainant_mobile'
-                    },
-                    {
-                        data: 'transaction_id'
-                    },
-                    {
-                        data: 'bank_name'
-                    },
-                    {
-                        data: 'account_id'
-                    },
-                    {
-                        data: 'amount'
-                    },
-                    {
-                        data: 'entry_date'
-                    },
-                    {
-                        data: 'current_status'
-                    },
-                    {
-                        data: 'date_of_action'
-                    },
-                    {
-                        data: 'action_taken_by_name'
-                    },
-                    {
-                        data: 'action_taken_by_designation'
-                    },
-                    {
-                        data: 'action_taken_by_mobile'
-                    },
-                    {
-                        data: 'action_taken_by_email'
-                    },
-                    {
-                        data: 'action_taken_by_bank'
-                    },
-                    {
-                        data: 'edit'
-                    }
-        ],
-        order: [0, 'desc'],
-        ordering: true
-    });
-
-    // Form submission event handler
-    $('#complaint-form').submit(function(event) {
-        event.preventDefault(); // Prevent default form submission
-
-        var from_date = $("#from-date").val();
-        var to_date = $("#to-date").val();
-        var mobile = $("#mobile").val();
-        var acknowledgement_no = $("#acknowledgement_no").val();
-        var filled_by = $("#filled-by").val();
-        var search_by = $("#search-by").val();
-        var options = $("#options").val();
-
-        // Construct the URL with query parameters
-        var url = "{{ route('get.datalist') }}?from_date=" + from_date + "&to_date=" + to_date + "&mobile=" + mobile + "&acknowledgement_no=" + acknowledgement_no + "&filled_by=" + filled_by + "&search_by=" + search_by + "&options=" + options;
-
-        // Reload DataTable with new data based on selected filters
-        table.ajax.url(url).load();
-    });
-
-
-
-});
-
-</script>
 @endsection
-
-
-
-
-
