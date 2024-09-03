b117c6956a73471f8205e5643e5f5915c1291579
Rajmohan-Kawika <raj@kawikatechnologies.com>
2024-09-03 14:51:55 +0530

    conflict resoleve

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -79,13 +79,13 @@ class ComplaintController extends Controller
                 if ($file){
                     try {
 
-                         FacadesExcel::import(new ComplaintImport($source_type), $file);
-                        // $userId = Auth::user()->id;
-                        // $uploadId = (string) Str::uuid();
-                        // session()->forget('upload_id');
-                        // session()->put('upload_id', $uploadId);
-                        // $filePath = $request->file('complaint_file')->store('imports');
-                        // ImportComplaintsJob::dispatch($filePath, $source_type , $userId , $uploadId);
+                        // FacadesExcel::import(new ComplaintImport($source_type), $file);
+                        $userId = Auth::user()->id;
+                        $uploadId = (string) Str::uuid();
+                        session()->forget('upload_id');
+                        session()->put('upload_id', $uploadId);
+                        $filePath = $request->file('complaint_file')->store('imports');
+                        ImportComplaintsJob::dispatch($filePath, $source_type , $userId , $uploadId);
 
                         // return redirect()->back()->with('success', 'Uploading...')->with('redirected', true);
                         return redirect()->back()->with('redirected', true);
